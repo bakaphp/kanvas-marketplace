ffc3106db6461e5035d5a66a5fb0583bc27e053d
RivierGrullon <grullonrivier@gmail.com>
2024-05-06 12:48:18 -0400

    feat: Add SortBy and Stats widgets for search results

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,39 @@
 'use client';
 import Hit from '@/components/molecules/hit';
-import { Configure, Hits, Pagination } from 'react-instantsearch';
+import {
+  Configure,
+  Hits,
+  Pagination,
+  SortBy,
+  Stats,
+} from 'react-instantsearch';
 
 export default function SearchPage() {
   return (
     <div>
-      <Configure hitsPerPage={9}/>
+      <Configure hitsPerPage={9} />
+      <div className='flex justify-between w-11/12 pb-2'>
+        <Stats
+          translations={{
+            rootElementText({
+              nbHits,
+              processingTimeMS,
+              nbSortedHits,
+              areHitsSorted,
+            }) {
+              return areHitsSorted && nbHits !== nbSortedHits
+                ? `${nbSortedHits!.toLocaleString()} relevant results sorted out of ${nbHits.toLocaleString()}`
+                : `${nbHits.toLocaleString()} results`;
+            },
+          }}
+        />
+        <SortBy
+          items={[
+            { value: '', label: 'Most Relevant' },
+            { value: 'product-asc', label: 'A-Z' },
+          ]}
+        />
+      </div>
       <Hits
         hitComponent={Hit}
         classNames={{ list: 'grid grid-cols-1 md:grid-cols-3 gap-4' }}
