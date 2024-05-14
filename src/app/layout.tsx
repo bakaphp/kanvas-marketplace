6445a5ab4c78142d50b346d5d1a46d2d261d32d1
RivierGrullon <grullonrivier@gmail.com>
2024-05-14 09:51:53 -0400

    style: Remove unnecessary comment and improve font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, Suspense } from 'react';
 import ClientLayout from './client';
 import Navbar from '@/components/organism/navbar';
-// import { Open_Sans } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import { ServerCoreStore } from '@kanvas/phoenix';
 import { app } from '@/models/services/kanvas';
@@ -22,7 +22,9 @@ export const metadata = {
   },
 };
 
-
+const OpenSans = Open_Sans({
+  subsets: ['latin'],
+});
 
 export default async function RootLayout({
   children,
@@ -30,7 +32,7 @@ export default async function RootLayout({
   children: ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={OpenSans.className}>
       <ServerCoreStore sdk={app}>
         <body className='bg-neutral-50 text-black selection:bg-teal-300 dark:bg-primary-background dark:text-white dark:selection:bg-pink-500 dark:selection:text-white'>
           <ClientLayout>
