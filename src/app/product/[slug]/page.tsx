ddc799bc59f73af1d43ab994ba33918cf6a1b668
RivierGrullon <grullonrivier@gmail.com>
2024-05-14 13:48:39 -0400

    style: Improve code formatting and add Suspense components

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { Suspense } from 'react';
 
 export const runtime = 'edge';
 
+
 async function useProductPage(params: any) {
   const { sdk } = useServerContext();
   const product = await sdk?.inventory.getProduct({
@@ -15,7 +16,7 @@ async function useProductPage(params: any) {
       value: params?.slug ?? '',
     },
   });
-
+  // console.log(product?.products.data[0].variants);
   return {
     models: {
       product,
@@ -25,46 +26,43 @@ async function useProductPage(params: any) {
 
 export default async function ProductPage({ params }: { params: any }) {
   const { models } = await useProductPage(params);
-  const productData = models.product?.products?.data?.[0];
-
-  if (!productData) {
-    return (
-      <div className='mx-auto max-w-screen-2xl px-4'>
-        <div className='flex flex-col w-5/6 rounded-lg border mx-auto border-neutral-200 bg-white p-8 md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-800 dark:bg-black'>
-          <div className='h-full w-full basis-full lg:basis-4/6'>
-            <div className='relative aspect-square h-full max-h-[550px] w-full overflow-hidden' />
-          </div>
-          <div className='basis-full lg:basis-2/6'>
-            <div>Loading...</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
       <div className='mx-auto max-w-screen-2xl px-4'>
         <div className='flex flex-col w-5/6 rounded-lg border mx-auto border-neutral-200 bg-white p-8 md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-800 dark:bg-black'>
           <div className='h-full w-full basis-full lg:basis-4/6'>
-            <Gallery
-              images={productData?.files?.data?.map((image) => ({
-                src: image.url,
-                altText: image.field_name,
-              }))}
-            />
+            <Suspense
+              fallback={
+                <div className='relative aspect-square h-full max-h-[550px] w-full overflow-hidden' />
+              }
+            >
+              <Gallery
+                // @ts-ignore
+                images={models.product?.products?.data?.[0]?.files?.data?.map(
+                  (image) => ({
+                    src: image.url,
+                    altText: image.field_name,
+                  })
+                )}
+              />
+            </Suspense>
           </div>
 
           <div className='basis-full lg:basis-2/6'>
-            <ProductDescription
-              product={{
-                title: productData.name,
-                descriptionHtml: productData.description,
-                variants: productData.variants,
-              }}
-            />
+            <Suspense fallback={null}>
+              <ProductDescription
+                product={{
+                  title: models.product?.products.data[0].name,
+                  descriptionHtml: models.product?.products.data[0].description,
+                  variants: models?.product?.products.data[0].variants,
+                }}
+              />
+            </Suspense>
           </div>
         </div>
+        {/* <Suspense>
+          <RelatedProducts id={product.id} />
+        </Suspense> */}
       </div>
     </>
   );
