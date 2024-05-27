9e21c5cb2d2af2b1b5b956e0cdd0937f40dca6d8
RivierGrullon <grullonrivier@gmail.com>
2024-05-27 17:13:59 -0400

    style: Update formatting in multiple files

--- src/components/organism/cart/add-to-cart.tsx
+++ src/components/organism/cart/add-to-cart.tsx
@@ -17,7 +17,7 @@ function SubmitButton({
 }) {
   const { pending } = useFormStatus();
   const buttonClasses =
-    'relative flex w-full items-center justify-center rounded-full bg-blue-600 p-4 tracking-wide text-white';
+    'mt-2 relative flex w-full items-center justify-center rounded-md bg-primary-100 p-3 tracking-wide text-white';
   const disabledClasses = 'cursor-not-allowed opacity-60 hover:opacity-60';
 
   if (!availableForSale) {
@@ -35,9 +35,6 @@ function SubmitButton({
         aria-disabled
         className={clsx(buttonClasses, disabledClasses)}
       >
-        <div className='absolute left-0 ml-4'>
-          <PlusIcon className='h-5' />
-        </div>
         Add To Cart
       </button>
     );
@@ -59,7 +56,8 @@ function SubmitButton({
         {pending ? (
           <LoadingDots className='mb-3 bg-white' />
         ) : (
-          <PlusIcon className='h-5' />
+          // <PlusIcon className='h-5' /> 
+          <></>
         )}
       </div>
       Add To Cart
