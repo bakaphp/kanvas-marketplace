bdc590da59adec82f87253a599a492d51fcad80a
RivierGrullon <grullonrivier@gmail.com>
2024-10-16 15:11:45 -0400

    feat: Add redirect from root to /search page

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/search',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
