df80b870b43b1410aabf26594053196dd43feefe
RivierGrullon <grullonrivier@gmail.com>
2024-10-17 13:26:38 -0400

    feat: Add useYupResolver hook for form validation

---
+++ src/hooks/useYupResolver/index.ts
@@ -0,0 +1,34 @@
+import { useCallback } from "react";
+
+export const useYupResolver = (validationSchema: any) =>
+  useCallback(
+    async (data: any) => {
+      try {
+        const values = await validationSchema.validate(data, {
+          abortEarly: false,
+        });
+
+        return {
+          values,
+          errors: {},
+        };
+      } catch (errors) {
+        return {
+          values: {},
+          // @ts-ignore
+          errors: errors.inner.reduce(
+            // @ts-ignore
+            (allErrors, currentError) => ({
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? "validation",
+                message: currentError.message,
+              },
+            }),
+            {}
+          ),
+        };
+      }
+    },
+    [validationSchema]
+  );
